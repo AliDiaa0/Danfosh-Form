391a1967a24d7a91e78885fe371bff78299fb52c
AliDiaa0 <85357930+AliDiaa0@users.noreply.github.com>
2022-02-19 14:35:27 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using Microsoft.Win32;
+
+namespace Danfosh
+{
+    public partial class Danfosh : Form
+    {
+        public Danfosh()
+        {
+            InitializeComponent();
+        }
+
+        private void Danfosh_Load(object sender, EventArgs e)
+        {
+            System.IO.Stream str = Properties.Resources.nokia_bulbul;
+            SoundPlayer snd = new System.Media.SoundPlayer(str);
+            snd.PlayLooping();
+        }
+    }
+}
